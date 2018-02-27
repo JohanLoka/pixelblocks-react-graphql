28b8df33bc9eb48ef5e617367dc69f89d837bb84
Johan Lindberg <joli@nao.local>
2018-02-27 10:26:21 +0100

    fixed map enemy force

--- client/components/MapItem.js
+++ client/components/MapItem.js
@@ -12,7 +12,8 @@ class MapItem extends Component {
     super(props);
 
     this.state = {
-      editing: false
+      editing: false,
+      force: this.props.data.enemy_force
     };
 
     this.editClicked = this.editClicked.bind(this);
@@ -48,66 +49,69 @@ class MapItem extends Component {
     this.setState({editing: false});
 
   }
-  submitClicked() {
-
-    console.log('submit Clicked');
+  onForceChanged() {
+    this.setState({force: this.refs.enemy_force.value});
   }
 
   renderForm() {
-    return (<div className="row grow">
-      <div className="col m12">
-        <div className="card blue-grey darken-1">
-          <div className="card-content white-text">
+    return (
+      <div className="row grow">
+        <div className="col m12">
+          <div className="card blue-grey darken-1">
+            <div className="card-content white-text">
 
-            <form onSubmit={this.onClickedSubmit.bind(this)}>
-              <div className="row">
-                <div className="input-field col m3">
-                  map_name
-                  <input value={this.props.data.map_name} name="map_name" id="map_name" type="text" className="white-text validate"/>
+              <form onSubmit={this.onClickedSubmit.bind(this)}>
+                <div className="row">
+                  <div className="input-field col m3">
+                    map_name
+                    <input value={this.props.data.map_name} name="map_name" id="map_name" type="text" className="white-text validate"/>
+                  </div>
+                  <div className="input-field col m3">
+                    enemy_force
+                    <input name="enemy_count" id="enemy_count" type="text" className="validate" value={this.state.force} onChange={this.onForceChanged.bind(this)} ref="enemy_force"/>
+                  </div>
+                  <div className="input-field col m3">
+                    reward_badges
+                    <input placeholder={this.props.data.reward_badges} name="enemy_count" id="enemy_count" type="text" className="validate"/>
+                  </div>
+                  <div className="input-field col m3">
+                    id
+                    <input value={this.props.data.id} ref="id" name="enemy_count" id="enemy_count" type="text" className="white-text validate" disabled="disabled"/>
+                  </div>
                 </div>
-                <div className="input-field col m3">
-                  enemy_force
-                  <input name="enemy_count" id="enemy_count" type="text" className="validate" placeholder={this.props.data.enemy_force} ref="enemy_force"/>
+                <div className="row">
+                  <div className="col m3">
+                    <button className="btn waves-effect waves-light pink" onClick={this.cancelClicked.bind(this)}>Cancel
+                      <i className="material-icons right">delete</i>
+                    </button>
+                  </div>
+                  <div className="col m3">
+                    <button className="btn waves-effect waves-light green">Update
+                      <i className="material-icons right">send</i>
+                    </button>
+                  </div>
                 </div>
-                <div className="input-field col m3">
-                  reward_badges
-                  <input placeholder={this.props.data.reward_badges} name="enemy_count" id="enemy_count" type="text" className="validate"/>
-                </div>
-                <div className="input-field col m3">
-                  id
-                  <input value={this.props.data.id} ref="id" name="enemy_count" id="enemy_count" type="text" className="white-text validate" disabled="disabled"/>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col m3">
-                  <button className="btn waves-effect waves-light pink" onClick={this.cancelClicked.bind(this)}>Cancel
-                    <i className="material-icons right">delete</i>
-                  </button>
-                </div>
-                <div className="col m3">
-                  <button className="btn waves-effect waves-light green">Update
-                    <i className="material-icons right">send</i>
-                  </button>
-                </div>
-              </div>
-            </form>
+              </form>
 
+            </div>
           </div>
         </div>
       </div>
-    </div>)
+    )
   }
 
   renderNormal() {
-    return (<div className="row grow" onClick={this.editClicked}>
-      <div className="col m12">
-        <div className="card blue-grey darken-1">
-          <div className="card-content white-text">
-            <span className="card-title">{this.props.data.map_name}</span>
+    return (
+      <div className="row grow" onClick={this.editClicked}>
+        <div className="col m12">
+          <div className="card blue-grey darken-1">
+            <div className="card-content white-text">
+              <span className="card-title">{this.props.data.map_name}</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>)
+    )
   }
   render() {
     if (this.state.editing)
